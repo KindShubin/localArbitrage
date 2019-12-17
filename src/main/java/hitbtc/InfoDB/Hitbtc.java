3e4d041470b2d6c7857ab8449a3a6169b62200c7
D Anoshin <anoshindmitriy@gmail.com>
2019-12-17 02:22:02 +0200

    add Classes for clone all info from DB Hitbtc1

--- src/main/java/hitbtc/InfoDB/Hitbtc.java
+++ src/main/java/hitbtc/InfoDB/Hitbtc.java
@@ -36,7 +36,15 @@ public class Hitbtc {
             double ask5 = GetVal.getDouble(hm,"ask5");
             Timestamp date = GetVal.getTimeS(hm,"date");
             String description = GetVal.getStr(hm,"description");
-            this.hitbtc.put(id, new HitbtcPair(id, pair, bid1, ask1, bid2, ask2, bid3, ask3, bid4, ask4, bid5, ask5, date, description));
+            HitbtcPair hbp = null;
+            try {
+                hbp = new HitbtcPair(id, pair, bid1, ask1, bid2, ask2, bid3, ask3, bid4, ask4, bid5, ask5, date, description);
+            } catch (Exception e){
+                e.printStackTrace();
+                System.out.println(e.toString());
+                System.out.println(e);
+            }
+            this.hitbtc.put(id, hbp);
         }
     }
 
